8823d6604cbf1194755c49a2383d620bed902540
Oladejo Jephthah <jephthahooh@gmail.com>
2024-12-19 11:20:30 +0100

    latest

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,19 +3,22 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
@@ -26,7 +29,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -34,15 +37,22 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+
+
+    function posts() : HasMany {
+
+        return $this->hasMany(Post::class);
+        
     }
+
+
 }
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Media;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +20,33 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id'=>User::factory(),
+            'description'=>fake()->sentence(),
+            'location'=>fake()->city(),
+            'allow_commenting'=>fake()->boolean(),
+            'hide_like_view'=>fake()->boolean(),
+            'type'=>'post',
         ];
     }
+
+    function configure() : static {
+        
+        return $this->afterCreating( function(Post $post){
+
+            if ($post->type=='reel') {
+
+                //
+               Media::factory()->reel()->create(['mediable_type'=>get_class($post),'mediable_id'=>$post->id]);
+            } else {
+
+                Media::factory()->post()->create(['mediable_type'=>get_class($post),'mediable_id'=>$post->id]);
+
+            }
+            
+
+        }
+        );
+    }
+
+
 }
