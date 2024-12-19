ca83e0e11cea4c2f96698fa471850f1724bbcdd3
Oladejo Jephthah <jephthahooh@gmail.com>
2024-12-19 12:06:35 +0100

    latest

--- database/migrations/2024_12_19_102423_create_media_table.php
+++ database/migrations/2024_12_19_102423_create_media_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,7 +14,7 @@ return new class extends Migration
             $table->id();
             $table->morphs('mediable');
             $table->string('url');
-            $table->enum('mime',['image','video']);
+            $table->enum('mime', ['image', 'video']);
             $table->timestamps();
         });
     }
