8a07b5d5c5259e3a63503bfe42870a6cd66626d4
Oladejo Jephthah <jephthahooh@gmail.com>
2024-12-19 12:52:43 +0100

    latest

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,10 +2,29 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class Home extends Component
 {
+
+
+    public $posts;
+
+ 
+
+    function mount() {
+        
+
+
+        $this->posts= Post::latest()->get();
+
+
+       // dd( $this->posts);
+
+    }
+    
     public function render()
     {
         return view('livewire.home');
